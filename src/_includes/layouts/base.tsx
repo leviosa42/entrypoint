c3184e0104d642144626071c9000236c0cb0c891
leviosa42 <101305426+leviosa42@users.noreply.github.com>
2024-12-17 12:37:10 +0900

    fix: viewport

--- src/_includes/layouts/base.tsx
+++ src/_includes/layouts/base.tsx
@@ -31,6 +31,7 @@ export default (
     <html lang="ja">
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{header.title}</title>
 
         <meta name="supported-color-schemes" content="light dark" />
